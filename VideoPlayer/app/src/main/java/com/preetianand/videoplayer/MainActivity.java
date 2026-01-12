44969004fc12b21e2bc7a6b26a9196078913aab7
preetianand3 <preetipaschapuri@gmail.com>
2026-01-12 15:37:54 +0530

    BeerAdviser, Trail-1, and VideoPlayer Android project initial commit

---
+++ VideoPlayer/app/src/main/java/com/preetianand/videoplayer/MainActivity.java
@@ -0,0 +1,36 @@
+package com.preetianand.videoplayer;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        videoView = findViewById(R.id.video);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setMediaPlayer(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(Uri.parse("android.resource://"+ getPackageName() + "/" + R.raw.video));
+        //videoView.setVideoURI(Uri.parse("https://youtu.be/m7QuO0IsT7g?si=PA2MqVgwfqV6BpzI"));
+    }
+}
